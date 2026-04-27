b97c400f9397c2acf43e6d243a46676cfcb98dcc
Eymen Aydın <eymenaydin1324@gmail.com>
2026-04-27 22:55:10 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -81,11 +81,9 @@ char* response(char* command){
         resp[resp_size-1]='\0';
     }
     else if (cmd == 2){
-        res_str = "PONG";
         resp=malloc(sizeof(char)*9);
-        char* pos=resp; memcpy(pos,"+",1); pos+=1;
-        memcpy(pos,res_str,5); pos+=5;
-        memcpy(pos,"\r\n",2);  resp[8]='\0';
+        memcpy(resp,"+PONG\r\n",7);
+        resp[7]='\0';
     }
 
 
